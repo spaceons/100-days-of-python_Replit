1f6c318a74b6f2bda7dde44ccf2c261ef96235d6
spaceons <agrestpawel@gmail.com>
2025-05-26 16:14:24 +0200

    Update and rename if-else to if-else.py

---
+++ Day5/if-else.py
@@ -0,0 +1,7 @@
+imię = input("Jak się nazywasz? ")
+if imię == "Paweł":
+  print("Hej Królu!")
+  print("Będę służyć Ci do końca wszechświata!!!")
+else:
+  print("Hej", imię, "szkoda, że muszę z Tobą rozmawiać...")
+  print("No cóż, w czym mogę pomóc?")
