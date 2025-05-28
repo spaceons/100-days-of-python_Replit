5da98b1068fcc204c41965e24121b3dee0716532
spaceons <agrestpawel@gmail.com>
2025-05-28 13:32:09 +0200

    Create fav_serial-if_in_if.py

---
+++ Day7/fav_serial-if_in_if.py
@@ -0,0 +1,14 @@
+favserial = input("Jaki jest Twój ulubiony serial? ")
+if favserial == "Picky Blinders":
+  print("Masz rację, ten serial jest świetny! ")
+  favCharacter = input("Ulubiona postać? > ")
+  if favCharacter == "Tommy":
+    print("Like a boss")
+  elif favCharacter == "Arthur":
+    print("The eldest son, respect")
+  else:
+    print("Meh")
+elif favserial == "Breaking Bad":
+  print("Haisenbergggg")
+else:
+  print("a tam, nie znasz się")
