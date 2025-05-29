9128ac60c196fdc99e977dbf4e5406bfffa02a31
Paweł Pawlak <agrestpawel@gmail.com>
2025-05-29 14:38:03 +0200

    Create challenge.py

---
+++ Day9/challenge.py
@@ -0,0 +1,13 @@
+print("=== Oto Identyfikator Generacji ===")
+print()
+y = int(input("W którym roku się urodziłęś? > "))
+if y >= 1925 and y <= 1946:
+  print("Tradycjonalista!")
+elif y >= 1947 and y <= 1964:
+  print("Baby Boomer!")
+elif y >= 1965 and y <= 1981:
+  print("Generacja X!")
+elif y >= 1982 and y <= 1995:
+  print("Milenials :D")
+elif y >= 1996 and y <= 2015:
+  print("ZETKA")
