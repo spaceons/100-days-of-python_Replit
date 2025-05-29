f8ec69d4ec8f9908dd503388d9996b4a62e4f0b5
Paweł Pawlak <agrestpawel@gmail.com>
2025-05-29 14:35:34 +0200

    Create int-float.py

---
+++ Day9/int-float.py
@@ -0,0 +1,21 @@
+myScore = int (input("Your score: "))
+if myScore > 10:
+  print("Winner!")
+else:
+  print("Try again 😭")
+
+# int = whole number
+# float = decimal (np. 3,14)
+
+myScore = float (input("What is Pi to 3dp? > "))
+if myScore == 3.142:
+  print("Good answear!")
+else:
+  print("Try again 😭")
+
+## dla IF:
+# równe ==
+# większe >
+# mniejsze <
+# większe lub równe >= (analogicznie z mniejszym lub równym <=)
+# !=
