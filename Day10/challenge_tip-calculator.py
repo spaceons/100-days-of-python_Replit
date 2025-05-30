793133feb0a7576f3133fdfd92b98495718f9182
Paweł Pawlak <agrestpawel@gmail.com>
2025-05-30 11:09:12 +0200

    Update challenge_tip-calculator.py

--- Day10/challenge_tip-calculator.py
+++ Day10/challenge_tip-calculator.py
@@ -1,1 +1,15 @@
+print("Tip Calculator")
+print()
+expense = float(input("""Ile wydaliście? > """))
+tip_proc = float(input("""Jaki procent chcesz napiwkować (XD)? > """))
 
+tip_number = tip_proc / 100 * expense + expense
+
+ppl = int(input("""Ilu Was jest? > """))
+naGlowe = tip_number / ppl
+naGlowe = round(naGlowe, 2)
+
+print("Suma wynosi: ", tip_number)
+print("Każdy musi dać: ", naGlowe)
+
+# były małe problemy, ale ogarnąłem temat
