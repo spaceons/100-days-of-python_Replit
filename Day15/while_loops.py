eae61f1d8739f63fe1eb1433cc97cf921ae4bedf
Paweł Pawlak <agrestpawel@gmail.com>
2025-06-01 10:59:28 +0200

    Update while_loops.py

--- Day15/while_loops.py
+++ Day15/while_loops.py
@@ -1,7 +1,16 @@
+# infinite loop:
+counter = 0
+while counter < 10:
+  print(counter)
+
+# fixed:
+counter = 0
+while counter < 10:
+  print(counter)
+  counter += 1
+
 # dlaczego ""?
 exit = ""
 while exit = "yes":
   print("🥳")
 exit = input("Exit?: ")
-
-# 
