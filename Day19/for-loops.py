831c4607bbefd88b3dd2d7ea8191854eb5214d1a
Paweł Pawlak <agrestpawel@gmail.com>
2025-06-04 11:30:04 +0200

    Update for-loops.py

--- Day19/for-loops.py
+++ Day19/for-loops.py
@@ -1,1 +1,7 @@
+debt = float(input("Enter you debt here > "))
+apr = float(input("Enter the APR here > "))
+years = int(input("How many years will you be repaying the loan? > "))
 
+for j in range(years):
+  debt += debt*apr
+  print("Year",j+1,"is", round(debt,2))
