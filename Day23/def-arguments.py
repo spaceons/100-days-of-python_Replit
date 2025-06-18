ab883f0e05c77d9754b82b4975c23543a49955dd
Paweł Pawlak <agrestpawel@gmail.com>
2025-06-18 15:07:24 +0200

    Update def-arguments.py

--- Day23/def-arguments.py
+++ Day23/def-arguments.py
@@ -1,15 +1,1 @@
-def biggerNumber(number1, number2):
-  if(number1 > number2):
-    print(number1, "is bigger")
-  elif number1 == number2:
-    print("Są równe!")
-  else:
-    print(number2, "is bigger")
 
-userNumber1 = float(input("Podaj pierwszy nr > "))
-userNumber2 = float(input("Podaj drugi nr > "))
-
-userNumber1 = round(userNumber1,2)
-userNumber2 = round(userNumber2,2)
-
-biggerNumber(userNumber1,userNumber2)
